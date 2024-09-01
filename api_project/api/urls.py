3f46c31462e568ba98908b0ae5f93555d04f26e5
KON-R4D <tyanlearner@gmail.com>
2024-09-01 12:48:38 +0300

    added CRUD operations to Book Model

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,20 @@
-from django.urls import path
-from .views import BookList
+#from django.urls import path
+#from .views import BookList
 
+#urlpatterns = [
+#    path('books/', BookList.as_view(), name='book-list'),  # URL for accessing the BookList view
+#]
+
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet
+
+# Create a router and register the BookViewSet
+router = DefaultRouter()
+router.register(r'books', BookViewSet)
+
+# Include the router URLs
 urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'),  # URL for accessing the BookList view
+    path('', include(router.urls)),  # Include all generated URL patterns
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,18 @@
-from rest_framework import generics
+#from rest_framework import generics
+#from .models import Book
+#from .serializers import BookSerializer
+
+# Create your views here.
+#class BookList(generics.ListAPIView):
+#    queryset = Book.objects.all()  # Retrieve all books from the database
+#    serializer_class = BookSerializer  # Use BookSerializer to serialize the data
+
+
+# Update the View with a ViewSet
+from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
 
-# Create your views here.
-class BookList(generics.ListAPIView):
-    queryset = Book.objects.all()  # Retrieve all books from the database
-    serializer_class = BookSerializer  # Use BookSerializer to serialize the data
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()  # Retrieve all Book objects from the database
+    serializer_class = BookSerializer  # Use BookSerializer for data serialization
